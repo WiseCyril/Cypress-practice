b399761d28bcc28e58181f88467dcfed221cf7ff
wiseCyril <unnamadim@getcarbon.co>
2019-09-19 11:43:27 +0100

    remove class from page object

--- cypress/integration/todomvc-actions.spec.js
+++ cypress/integration/todomvc-actions.spec.js
@@ -1,34 +1,45 @@
 /// <reference types="cypress"/>
 
-import { TodoPage } from "../page-objects/todo-page";
+import { 
+    navigate,
+    addTodo,
+    toggleTodo,
+    showOnlyCompletedTodos,
+    showOnlyActiveTodos,
+    showAllTodos,
+    clearCompleted,
+    validateNumberOfTodosShown,
+    validateTodoCompletedState,
+    validateTodoText,
+    validateToggleState 
+} from "../page-objects/todo-page";
 
 describe('todo actions', () => {
-    const todoPage = new TodoPage()
 
     beforeEach(() => {
-        todoPage.navigate()
+        navigate()
 
-        todoPage.addTodo('Learn Cypress')
+        addTodo('Learn Cypress')
     })
 
     it.only('should be ale to add a new todo to the list', () => {
-        todoPage.validateTodoText(0, 'Learn Cypress')
+        validateTodoText(0, 'Learn Cypress')
         
-        todoPage.validateToggleState(0, false)
+        validateToggleState(0, false)
     })
 
     describe('toggling todos', () => {
         it('should toggle test correctly', () => {
-          todoPage.toggleTodo(0)
-          todoPage.validateTodoCompletedState(0, true)
+          toggleTodo(0)
+          validateTodoCompletedState(0, true)
         })
 
         it('should clear completed', () => {
-            todoPage.toggleTodo(0)
+            toggleTodo(0)
       
-            todoPage.clearCompleted()
+            clearCompleted()
       
-            todoPage.validateNumberOfTodosShown(0)
+            validateNumberOfTodosShown(0)
         })
     })
 })
